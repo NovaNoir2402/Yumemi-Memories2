808bfd9cb2b70eca1931f96ae1596d06706a7134
Truong Khac Minh Tien <khac-minh-tien.truong@etudiant.univ-rennes.fr>
2025-05-08 17:49:13 +0200

    Added placeholder player model from assets.

--- src/level.ts
+++ src/level.ts
@@ -10,20 +10,20 @@ export class Level {
         this._scene = scene;
     }
 
-    public generateSimpleGrid(gridSize: number, roomSize: Vector3): void {
-        const environment = new Environment(scene);
-                const room1 = environment.createRoom("Room1", ROOM_SIZE, ROOM1_POSITION);
-                const room2 = environment.createRoom("Room2", ROOM_SIZE, ROOM2_POSITION);
-                environment.createExit(room1, room2, "south");
-        const spacing = roomSize.add(new Vector3(2, 0, 2)); // Add some space between rooms
-        for (let x = 0; x < gridSize; x++) {
-            for (let z = 0; z < gridSize; z++) {
-                const center = new Vector3(x * spacing.x, 0, z * spacing.z);
-                const room = new Room(`Room_${x}_${z}`, roomSize, center, this._scene);
-                this._rooms.push(room);
-            }
-        }
-    }
+    // public generateSimpleGrid(gridSize: number, roomSize: Vector3): void {
+    //     const environment = new Environment(scene);
+    //             const room1 = environment.createRoom("Room1", ROOM_SIZE, ROOM1_POSITION);
+    //             const room2 = environment.createRoom("Room2", ROOM_SIZE, ROOM2_POSITION);
+    //             environment.createExit(room1, room2, "south");
+    //     const spacing = roomSize.add(new Vector3(2, 0, 2)); // Add some space between rooms
+    //     for (let x = 0; x < gridSize; x++) {
+    //         for (let z = 0; z < gridSize; z++) {
+    //             const center = new Vector3(x * spacing.x, 0, z * spacing.z);
+    //             const room = new Room(`Room_${x}_${z}`, roomSize, center, this._scene);
+    //             this._rooms.push(room);
+    //         }
+    //     }
+    // }
 
     public getRooms(): Room[] {
         return this._rooms;
