ccf71daabf9dcf4062f1d5d289e93011eb5f715e
Truong Khac Minh Tien <khac-minh-tien.truong@etudiant.univ-rennes.fr>
2025-05-13 06:13:40 +0200

    Adding deploy page

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const appDirectory = fs.realpathSync(process.cwd());
 module.exports = {
     entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
     output: {
+        path: path.resolve(appDirectory, "dist"),
         filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
         clean: true,
     },
